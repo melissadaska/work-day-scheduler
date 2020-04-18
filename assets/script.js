ba5071e77fd0c55bb2b99dea849a5f4000c4ccad
Melissa Daskalantonakis <melissa.daska@gmail.com>
2020-04-18 09:46:36 -0500

    add button click and switch statement to concert hour string into hour number

--- assets/script.js
+++ assets/script.js
@@ -31,4 +31,27 @@ for(const property in workDay) {
     counter ++;
 }
 
+// call saveSchedule when button is clicked
+$("button").click(function() {
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
+
+    // call saveSchedule
+});
+
+// switch statement to convert hour string into hour number
+function hourNumberFromHourString(hourString) {
+    switch(hourString) {
+        case "7 AM": return 7;
+        case "8 AM": return 8;
+        case "9 AM": return 9;
+        case "10 AM": return 10;
+        case "11 AM": return 11;
+        case "12 PM": return 12;
+        case "1 PM": return 13;
+        case "2 PM": return 14;
+        case "3 PM": return 15;
+        case "4 PM": return 16;
+    }
+}
 
